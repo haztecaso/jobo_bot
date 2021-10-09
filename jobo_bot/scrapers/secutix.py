a7851d291fe2a4d9aa8c90a7fd0ef91b2c0e9e55
haztecaso <adrianlattes@disroot.org>
2021-10-09 04:03:48 +0200

    reordenando

--- jobo_bot/scrapers/secutix.py
+++ jobo_bot/scrapers/secutix.py
@@ -11,6 +11,59 @@ LOGIN_URL  = f"{BASE_URL}/account/login"
 EVENTS_URL = f"{BASE_URL}/secured/list/events"
 
 
+class Scraper():
+    def __init__(self, config:Config):
+        self.config = config
+        self.username = config.jobo_user
+        self.password = config.jobo_password 
+        self.session = requests.session()
+        self.logger = config.logger
+        self._token = None
+        self.login()
+
+    def get_token(self):
+        self.logger.debug("Getting csrf token")
+        result = self.session.get(LOGIN_URL)
+        soup = BeautifulSoup(result.text, 'html.parser')
+        return soup.find('input', {'name': '_csrf'}).get('value')
+
+    @property
+    def token(self):
+        if not self._token:
+            self._token = self.get_token()
+        return self._token
+
+    def login(self):
+        self.logger.debug("logging in")
+        payload = {
+                'login': self.username,
+                'password': self.password,
+                '_rememberThisLogin': 'on',
+                '_csrf': self.token
+                }
+        return self.session.post(
+                LOGIN_URL,
+                data=payload,
+                headers=dict(referer=LOGIN_URL)
+                )
+
+    def _get_events_raw(self):
+        self.logger.debug("Getting events")
+        html = self.session.get(
+                EVENTS_URL,
+                headers = dict(referer=EVENTS_URL)
+                ).text
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup.select('div.group_content > ul > li > .product')
+
+    def fetch_events(self) -> List[Event]:
+        events_raw = self._get_events_raw()
+        self.logger.debug("Scraping events")
+        self.logger.debug(f"{len(events_raw)} events found")
+        return [EventScraper(self.config).scrape(event_raw)\
+                for event_raw in events_raw]
+
+
 class EventScraper:
     def __init__(self, conf: Config):
         self.config = conf
@@ -52,7 +105,7 @@ class EventScraper:
 
     def _scrape_id(self):
         return self._soup.get('id').replace('prod_','')
- 
+
     def _scrape_title(self):
         return self._scrape_selector('.title')
 
@@ -78,57 +131,4 @@ class EventScraper:
         rel_url = self._scrape_href('span.button a')
         url = BASE_URL+str(rel_url) if rel_url else None
         return url
-
-
-class Scraper():
-    def __init__(self, config:Config):
-        self.config = config
-        self.username = config.jobo_user
-        self.password = config.jobo_password 
-        self.session = requests.session()
-        self.logger = config.logger
-        self._token = None
-        self.login()
-
-    def get_token(self):
-        self.logger.debug("Getting csrf token")
-        result = self.session.get(LOGIN_URL)
-        soup = BeautifulSoup(result.text, 'html.parser')
-        return soup.find('input', {'name': '_csrf'}).get('value')
-
-    @property
-    def token(self):
-        if not self._token:
-            self._token = self.get_token()
-        return self._token
-
-    def login(self):
-        self.logger.debug("logging in")
-        payload = {
-                'login': self.username,
-                'password': self.password,
-                '_rememberThisLogin': 'on',
-                '_csrf': self.token
-                }
-        return self.session.post(
-                LOGIN_URL,
-                data=payload,
-                headers=dict(referer=LOGIN_URL)
-                )
-
-    def _get_events_raw(self):
-        self.logger.debug("Getting events")
-        return self.session.get(
-                EVENTS_URL,
-                headers = dict(referer=EVENTS_URL)
-                ).text
-
-    def fetch_events(self) -> List[Event]:
-        html = self._get_events_raw()
-        self.logger.debug("Scraping events")
-        soup = BeautifulSoup(html, 'html.parser')
-        events_raw = soup.select('div.group_content > ul > li > .product')
-        self.logger.debug(f"{len(events_raw)} events found")
-        return [EventScraper(self.config).scrape(event_raw)\
-                for event_raw in events_raw]
 
