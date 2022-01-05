a67b9d6ebe146a6681726015276807e045350484
haztecaso <adrianlattes@disroot.org>
2022-01-05 14:04:48 +0100

    moviendo shell a flake.nix

--- flake.nix
+++ flake.nix
@@ -12,7 +12,20 @@
           jobo_bot = import ./default.nix { inherit pkgs; };
         };
         defaultPackage = packages.jobo_bot;
-        devShell = import ./shell.nix { inherit pkgs; };
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs.python38Packages; [
+            jobo_bot
+            mypy
+            requests
+            beautifulsoup4
+            python-telegram-bot
+            selenium
+            sqlalchemy
+          ];
+          shellHook = ''
+            alias jobo_bot_="python jobo_bot"
+          '';
+        };
       }
     );
 }
