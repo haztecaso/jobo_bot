96a7307fc33ef1a266a7daac3cdaced2902b1511
haztecaso <adrianlattes@disroot.org>
2022-01-05 15:37:21 +0100

    utilizando chromedriver en lugar de firefox. actualizando flake

--- flake.nix
+++ flake.nix
@@ -6,22 +6,41 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      requirements = with pkgs.python38Packages; [
+        requests
+        beautifulsoup4
+        python-telegram-bot
+        selenium
+        sqlalchemy
+        pkgs.chromium
+        pkgs.chromedriver
+      ];
+    in
       rec {
         packages = flake-utils.lib.flattenTree {
-          jobo_bot = import ./default.nix { inherit pkgs; };
+          jobo_bot = pkgs.python38Packages.buildPythonPackage rec {
+            pname = "jobo_bot";
+            version = "1.2.0";
+          
+            src = ./.;
+          
+            propagatedBuildInputs = requirements;
+          
+            meta = with pkgs.lib; {
+              homepage = "https://github.com/haztecaso/jobo_bot";
+              description = "Bot para recibir avisos de los eventos de jobo";
+              license = licenses.gpl3;
+            };
+          };
         };
         defaultPackage = packages.jobo_bot;
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs.python38Packages; [
-            # jobo_bot
+          nativeBuildInputs = requirements ++ (with pkgs.python38Packages; [
+            # packages.jobo_bot
             mypy
-            requests
-            beautifulsoup4
-            python-telegram-bot
-            selenium
-            sqlalchemy
-          ];
+          ]);
           shellHook = ''
             alias jobo_bot_="python jobo_bot"
           '';
