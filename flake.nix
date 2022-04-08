b37d1e167d2641258d6f426c6aae65084a9315fa
Adrián Lattes <adrianlattes@disroot.org>
2022-04-08 02:30:19 +0200

    refactoring flake

--- flake.nix
+++ flake.nix
@@ -1,48 +1,47 @@
 {
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs";
-  };
+  description = "Bot para recibir avisos de los eventos de jobo.";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      requirements = with pkgs.python38Packages; [
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "aarch64-linux" "aarch64-darwin" "i686-linux" "x86_64-darwin" "x86_64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+    requirements = python38Packages: with python38Packages; [
         requests
         beautifulsoup4
         python-telegram-bot
         sqlalchemy
-      ];
-    in
-      rec {
-        packages = flake-utils.lib.flattenTree {
-          jobo_bot = pkgs.python38Packages.buildPythonPackage rec {
-            pname = "jobo_bot";
-            version = "1.2.0";
-          
-            src = ./.;
-          
-            propagatedBuildInputs = requirements;
-          
-            meta = with pkgs.lib; {
-              homepage = "https://github.com/haztecaso/jobo_bot";
-              description = "Bot para recibir avisos de los eventos de jobo";
-              license = licenses.gpl3;
-            };
-          };
-        };
-        defaultPackage = packages.jobo_bot;
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = requirements ++ (with pkgs; [
-            # packages.jobo_bot
-            jq
-            fx
-          ]);
-          shellHook = ''
-            alias jobo_bot_="python jobo_bot"
-          '';
-        };
-      }
-    );
+    ];
+
+    jobo_bot = {lib, python38Packages}: python38Packages.buildPythonPackage rec {
+      pname = "jobo_bot";
+      version = "1.2.0";
+
+      src = ./.;
+
+      propagatedBuildInputs = requirements python38Packages;
+
+      meta = {
+        homepage = "https://github.com/haztecaso/jobo_bot";
+        description = "Bot para recibir avisos de los eventos de jobo";
+        license = lib.licenses.gpl3;
+      };
+    };
+  in rec {
+    packages = forAllSystems (system: {
+      jobo_bot = nixpkgs.legacyPackages.${system}.callPackage jobo_bot { };
+    });
+
+    defaultPackage = forAllSystems (system: packages.${system}.jobo_bot);
+
+    devShell = forAllSystems (system: nixpkgs.legacyPackages.${system}.mkShell {
+      nativeBuildInputs = with nixpkgs.legacyPackages.${system};
+        requirements python38Packages ++ [ jq fx ];
+      shellHook = ''
+        alias jobo_bot_="python jobo_bot"
+      '';
+    });
+  };
 }
